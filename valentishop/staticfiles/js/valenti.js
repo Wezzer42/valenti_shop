85e848fb3f102b90f2f8a63171bcce2a36e3cf65
Wezzer42 <alibaev.am5@gmail.com>
2025-01-29 15:53:43 +0900

    удалены static, staticfiles, mediafiles

--- valentishop/staticfiles/js/valenti.js
+++
@@ -1,9 +0,0 @@
-// Подсветка активного элемента при выборе
-document.querySelectorAll('.radio-option input').forEach(radio => {
-    radio.addEventListener('change', function () {
-        document.querySelectorAll('.radio-option').forEach(option => {
-            option.classList.remove('active');
-        });
-        this.parentElement.classList.add('active');
-    });
-});
